8fd1aed751cc420477077501903fba16ed1bb4c6
KchChr <111366979+KchChr@users.noreply.github.com>
2026-02-27 16:59:53 +0100

    iteration

--- tails-pdp-ebpf/src/main.rs
+++ tails-pdp-ebpf/src/main.rs
@@ -1,13 +1,14 @@
 #![no_std]
 #![no_main]
 
-use aya_ebpf::{macros::lsm, programs::LsmContext};
-use aya_log_ebpf::info;
+use aya_ebpf::{EbpfContext, macros::lsm, programs::LsmContext};
 
 #[lsm(hook = "file_open")]
 pub fn file_open(ctx: LsmContext) -> i32 {
     // Phase 1: minimal verifier-friendly program.
-    //info!(&ctx, "tails-pdp: file_open hook called");
+    unsafe {
+        aya_ebpf::bpf_printk!(b"tails-pdp: file_open pid=%d", ctx.pid());
+    }
     0
 }
 
@@ -20,4 +21,3 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
 #[unsafe(link_section = "license")]
 #[unsafe(no_mangle)]
 static LICENSE: [u8; 13] = *b"Dual MIT/GPL\0";
-
