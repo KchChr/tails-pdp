9127489c321f64a8bef5f68b7826df107992b54c
KchChr <111366979+KchChr@users.noreply.github.com>
2026-02-27 18:55:27 +0100

    iteration

--- tails-pdp-ebpf/src/main.rs
+++ tails-pdp-ebpf/src/main.rs
@@ -62,10 +62,18 @@ pub fn combine(ctx: LsmContext) -> i32 {
         aya_ebpf::bpf_printk!(b"tails-pdp: combine");
     }
     match DECISIONS.get(0) {
-        Some(v) if *v != 0 => -1, // deny
-        _ => 0,                   // permit/default
+        Some(v) if *v != 0 => {
+            unsafe {
+                aya_ebpf::bpf_printk!(b"DENY");
+            }
+            -1}, // deny
+        _ => {
+            unsafe {
+                aya_ebpf::bpf_printk!(b"PERMIT");
+            }
+            0},                   // permit/default
     }
-    
+
 }
 
 
