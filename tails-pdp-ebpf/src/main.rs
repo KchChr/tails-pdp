e23787f0790237a4ce066eef74515e62a88495cc
KchChr <111366979+KchChr@users.noreply.github.com>
2026-02-27 16:43:57 +0100

    iteration

--- tails-pdp-ebpf/src/main.rs
+++ tails-pdp-ebpf/src/main.rs
@@ -1,14 +1,13 @@
 #![no_std]
 #![no_main]
 
-use aya_ebpf::{EbpfContext, macros::lsm, programs::LsmContext};
+use aya_ebpf::{macros::lsm, programs::LsmContext};
+use aya_log_ebpf::info;
 
 #[lsm(hook = "file_open")]
 pub fn file_open(ctx: LsmContext) -> i32 {
     // Phase 1: minimal verifier-friendly program.
-    unsafe {
-        aya_ebpf::bpf_printk!(b"tails-pdp: file_open pid=%d", ctx.pid());
-    }
+    info!(&ctx, "tails-pdp: file_open hook called");
     0
 }
 
