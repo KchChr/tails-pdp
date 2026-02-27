9f35de63f9661f48e5441a533303724e50dfbf37
KchChr <kch.chrstn@gmail.com>
2026-02-27 14:36:01 +0100

    init

---
+++ tails-pdp-ebpf/src/main.rs
@@ -0,0 +1,28 @@
+#![no_std]
+#![no_main]
+
+use aya_ebpf::{macros::lsm, programs::LsmContext};
+use aya_log_ebpf::info;
+
+#[lsm(hook = "file_open")]
+pub fn file_open(ctx: LsmContext) -> i32 {
+    match try_file_open(ctx) {
+        Ok(ret) => ret,
+        Err(ret) => ret,
+    }
+}
+
+fn try_file_open(ctx: LsmContext) -> Result<i32, i32> {
+    info!(&ctx, "lsm hook file_open called");
+    Ok(0)
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+#[unsafe(link_section = "license")]
+#[unsafe(no_mangle)]
+static LICENSE: [u8; 13] = *b"Dual MIT/GPL\0";
