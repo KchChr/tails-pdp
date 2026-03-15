701aaf42493bcdcf632d54c140a89eec99fa6347
KchChr <111366979+KchChr@users.noreply.github.com>
2026-03-15 20:05:54 +0100

    iteration

--- tails-pdp-ebpf/src/policies/evaluate_stream_policies.rs
+++ tails-pdp-ebpf/src/policies/evaluate_stream_policies.rs
@@ -3,7 +3,7 @@ use aya_ebpf::{macros::lsm, programs::LsmContext};
 use crate::maps::{COMBINE, POLICY_JUMP_TABLE};
 
 #[lsm(hook = "file_open")]
-pub fn evaluate_stram_policies(ctx: LsmContext) -> i32 {
+pub fn evaluate_stream_policies(ctx: LsmContext) -> i32 {
     unsafe {
         aya_ebpf::bpf_printk!(b"tails-pdp: evaluate_stram_policies");
         let _ = POLICY_JUMP_TABLE.tail_call(&ctx, COMBINE);
--- tails-pdp-ebpf/src/policies/mod.rs
+++ tails-pdp-ebpf/src/policies/mod.rs
@@ -1,3 +1,3 @@
 pub mod combine;
 pub mod evaluate_static_policys;
-pub mod evaluate_stram_policies;
+pub mod evaluate_stream_policies;
