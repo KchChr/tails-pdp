8c0bed9067f72faf5aaebc99a22c640c67a60363
KchChr <111366979+KchChr@users.noreply.github.com>
2026-03-28 13:19:24 +0100

    iteration

--- tails-pdp/src/main.rs
+++ tails-pdp/src/main.rs
@@ -67,7 +67,7 @@ async fn main() -> anyhow::Result<()> {
     .context("failed to open POLICY_JUMP_TABLE")?;
     let static_policies = [StaticPolicy::new(
         Entitlement::Deny,
-        1001,
+        1000,
         PolicyAction::FileOpen,
         "cat",
         "/home/hntr/test.txt",
