b5dd8e50c0108c9bb03398a682be73cd6e3e6157
KchChr <111366979+KchChr@users.noreply.github.com>
2026-04-05 16:08:37 +0200

    iteration

---
+++ remove_maps.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+sudo rm -f /sys/fs/bpf/tails-pdp/FILE_OPEN_STATIC_POLICIES
+sudo rm -f /sys/fs/bpf/tails-pdp/FILE_OPEN_STREAM_POLICIES
+sudo rm -f /sys/fs/bpf/tails-pdp/SOCKET_BIND_STATIC_POLICIES
+sudo rm -f /sys/fs/bpf/tails-pdp/SOCKET_BIND_STREAM_POLICIES
+sudo rm -f /sys/fs/bpf/tails-pdp/STATIC_POLICY
+sudo rm -f /sys/fs/bpf/tails-pdp/STREAM_POLICY
+sudo rm -f /sys/fs/bpf/tails-pdp/CURRENT_TIME
