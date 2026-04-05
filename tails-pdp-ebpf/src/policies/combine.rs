a7ee6fde158206db51ab086d433dd66ef74f10e9
KchChr <111366979+KchChr@users.noreply.github.com>
2026-04-05 13:58:54 +0200

    iteration

--- tails-pdp-ebpf/src/policies/combine.rs
+++ tails-pdp-ebpf/src/policies/combine.rs
@@ -9,28 +9,33 @@ fn combine_decision() -> i32 {
     }
 }
 
-#[lsm(hook = "file_open")]
-pub fn combine_file_open(_ctx: LsmContext) -> i32 {
-    let decision = combine_decision();
-    unsafe {
-        if decision != 0 {
-            aya_ebpf::bpf_printk!(b"file_open final=deny");
-        } else {
-            aya_ebpf::bpf_printk!(b"file_open final=permit");
+macro_rules! define_combine_program {
+    ($fn_name:ident, $hook:literal, $deny_log:literal, $permit_log:literal) => {
+        #[lsm(hook = $hook)]
+        pub fn $fn_name(_ctx: LsmContext) -> i32 {
+            let decision = combine_decision();
+            unsafe {
+                if decision != 0 {
+                    aya_ebpf::bpf_printk!($deny_log);
+                } else {
+                    aya_ebpf::bpf_printk!($permit_log);
+                }
+            }
+            decision
         }
-    }
-    decision
+    };
 }
 
-#[lsm(hook = "socket_bind")]
-pub fn combine_socket_bind(_ctx: LsmContext) -> i32 {
-    let decision = combine_decision();
-    unsafe {
-        if decision != 0 {
-            aya_ebpf::bpf_printk!(b"socket_bind final=deny");
-        } else {
-            aya_ebpf::bpf_printk!(b"socket_bind final=permit");
-        }
-    }
-    decision
-}
+define_combine_program!(
+    combine_file_open,
+    "file_open",
+    b"file_open final=deny",
+    b"file_open final=permit"
+);
+
+define_combine_program!(
+    combine_socket_bind,
+    "socket_bind",
+    b"socket_bind final=deny",
+    b"socket_bind final=permit"
+);
