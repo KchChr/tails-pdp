8e38ee75dcd57813bbecdc80d471ed972c9fb44a
KchChr <111366979+KchChr@users.noreply.github.com>
2026-04-12 15:37:32 +0200

    iteration

--- tails-pdp-admintool/src/cli.rs
+++ tails-pdp-admintool/src/cli.rs
@@ -242,12 +242,57 @@ pub fn print_usage() {
     println!(
         "      [--subject <UID>] [--attribute <time|hour|minute|second>] --operator <...> [--modulo <N>] --value <N>"
     );
+    println!(
+        "      operator: <less-than|less-than-or-equal|equal|greater-than-or-equal|greater-than>"
+    );
+    println!(
+        "      attribute=time: vergleicht current_time % modulo mit value; --modulo ist Pflicht."
+    );
+    println!("      attribute=hour: vergleicht die UTC-Stunde 0..23 direkt mit value.");
+    println!("      attribute=minute: vergleicht die UTC-Minute 0..59 direkt mit value.");
+    println!("      attribute=second: vergleicht die UTC-Sekunde 0..59 direkt mit value.");
     println!("      file-open: [--resource <PFAD>]");
     println!(
         "      socket-bind: [--family <any|inet|inet6>] [--transport <any|tcp|udp>] [--resource <IP>] [--port <N>]"
     );
     println!("  load-examples");
     println!("  load-stream-examples");
+    println!();
+    println!("OPTIONS:");
+    println!("  --file-open-static-pin-path <PFAD>");
+    println!("      Standard: /sys/fs/bpf/tails-pdp/FILE_OPEN_STATIC_POLICIES");
+    println!("  --file-open-stream-pin-path <PFAD>");
+    println!("      Standard: /sys/fs/bpf/tails-pdp/FILE_OPEN_STREAM_POLICIES");
+    println!("  --socket-bind-static-pin-path <PFAD>");
+    println!("      Standard: /sys/fs/bpf/tails-pdp/SOCKET_BIND_STATIC_POLICIES");
+    println!("  --socket-bind-stream-pin-path <PFAD>");
+    println!("      Standard: /sys/fs/bpf/tails-pdp/SOCKET_BIND_STREAM_POLICIES");
+    println!("  --action <file-open|socket-bind>");
+    println!("      Waehlt den Hook fuer clear, clear-stream, clear-all, set und set-stream.");
+    println!("  --entitlement <permit|deny>");
+    println!("      Entscheidung der Policy.");
+    println!("  --subject <UID>");
+    println!("      Subjekt der Policy; Standard ist ANY_SUBJECT.");
+    println!("  --command <NAME>");
+    println!("      Nur fuer file-open static: Prozessname wie z. B. cat oder tail.");
+    println!("  --resource <WERT>");
+    println!("      file-open: Dateipfad; socket-bind: IP-Adresse.");
+    println!("  --family <any|inet|inet6>");
+    println!("      Nur fuer socket-bind.");
+    println!("  --transport <any|tcp|udp>");
+    println!("      Nur fuer socket-bind.");
+    println!("  --port <N>");
+    println!("      Nur fuer socket-bind.");
+    println!("  --attribute <time|hour|minute|second>");
+    println!("      Nur fuer set-stream.");
+    println!(
+        "  --operator <less-than|less-than-or-equal|equal|greater-than-or-equal|greater-than>"
+    );
+    println!("      Vergleichsoperator fuer Stream-Policies.");
+    println!("  --modulo <N>");
+    println!("      Nur fuer attribute=time; vergleicht current_time % modulo mit value.");
+    println!("  --value <N>");
+    println!("      Vergleichswert fuer Stream-Policies.");
     println!();
     println!("BEISPIELE:");
     println!("  tails-pdp-admintool show");
